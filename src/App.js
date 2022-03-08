a7371938774bdfcdebe7693a249b133bd98780bf
Shahadat <shahadat2cp17@gmail.com>
2022-03-08 15:24:16 +0600

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({type: 'INC'})
+  };
+  const decrement = () => {
+    dispatch({type: 'DEC'})
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "redux";
+
+const reducerFn = (state = {counter: 10}, action) => {
+    if(action.type === 'INC'){
+        return { counter: state.counter + 1}
+    }
+    if(action.type === 'DEC'){
+        return { counter: state.counter - 1}
+    }
+    return state
+
+}
+
+const store = createStore(reducerFn);
+export default store;
