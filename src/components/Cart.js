c2599db1e5025f760396aca78c82a7a698b9a930
Shahadat <shahadat2cp17@gmail.com>
2022-03-13 10:27:19 +0600

    com

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../store/cart-clice";
 import "./Cart.css";
+
 const Cart = () => {
   const quantity = useSelector((state) => state.cart.totalQuantity);
   const dispatch = useDispatch();
